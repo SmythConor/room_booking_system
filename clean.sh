68da910216f327a65277329bde36769d22fdc071
Conor Smyth <cnrsmyth@gmail.com>
2016-04-18 19:23:32 +0100

    Added removing of xmls to clean

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 BIN=/home/conor/work/college/year4/room_booking_system/bin
-RES=/home/conor/work/college/year4/room_booking_system/src/main/resources/
+RES=/home/conor/work/college/year4/room_booking_system/src/main/resources
 
 rm -rf $BIN
 if [ -f "$RES" ]; then
